2bdc1610ffde157430469e261e50ff505ddbfee4
madiwa Simon <mad.simon01@gmail.com>
2020-09-29 00:17:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (x)
   
-  if x % 3 == 0
-    return "Fizz"
+  if x % 5 == 0 && x % 3 == 0
+    return "FizzBuzz"
   elsif x % 5 == 0
     return "Buzz"
-  elsif x % 5 == 0 && x % 3 == 0
-    return "FizzBuzz"
+  elsif x % 3 == 0
+    return "Fizz"
   else
     return
   end
